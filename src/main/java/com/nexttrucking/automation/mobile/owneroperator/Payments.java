62d2ba3276f626fa6673ed69368a6c32e4b9c515
SergiiNik <nikitin.sergii@nexttrucking.com>
2019-07-27 18:05:21 +0800

    Fixing SignUp tests

--- src/main/java/com/nexttrucking/automation/mobile/owneroperator/Payments.java
+++ src/main/java/com/nexttrucking/automation/mobile/owneroperator/Payments.java
@@ -1,4 +1,28 @@
 package com.nexttrucking.automation.mobile.owneroperator;
 
-public class Payments {
+import com.nexttrucking.automation.mobile.property.PageProperty;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+import java.util.HashMap;
+
+public class Payments extends PageProperty {
+    public Payments(AppiumDriver<MobileElement> driver, String attributeName) {
+        super(driver, attributeName);
+    }
+
+    public HashMap<String, String> paymentList;
+
+
+    {
+        paymentList=new HashMap<>();
+        if (attributeName.equals("text")) {
+            paymentList.put("filterButton", "//*[contains(@text,'\uF182')]");
+        } else {
+            paymentList.put("filterButton", "//XCUIElementTypeStaticText[@name=\"\uF182\"]");
+        }
+    }
+
+
+
 }
--- src/test/java/testcases/owneroperator/PaymentsTest.java
+++ src/test/java/testcases/owneroperator/PaymentsTest.java
@@ -6,12 +6,17 @@ import com.nexttrucking.automation.mobile.dispatcher.AllowLocationPage;
 import com.nexttrucking.automation.mobile.dispatcher.AvailableLoadsAllPage;
 import com.nexttrucking.automation.mobile.dispatcher.MyDriversPage;
 import com.nexttrucking.automation.mobile.property.PageProperty;
+import com.nexttrucking.automation.mobile.property.Utils;
+import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import property.SetProperty;
 
 import java.net.MalformedURLException;
 
-public class Payments extends SetProperty {
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+
+public class PaymentsTest extends SetProperty {
 
     @BeforeClass
     public static void setUp() throws MalformedURLException, InterruptedException {
@@ -24,8 +29,18 @@ public class Payments extends SetProperty {
         myDriversPage = new MyDriversPage(driver, attributeName);
         // SignIn as OwnerOperator
         signInPage.signIn(getTestData("ownerOperatorEmail"), getTestData("ownerOperatorPassword"));
-        availableLoadsAllPage.clickMenuButtonFirstLevel("Pa");
-        pageProperty.clickElementByName(myDriversPage.getAnyTitle(), "Edit");
+        availableLoadsAllPage.clickMenuButtonFirstLevel("Payments");
+    }
+
+    @Test
+    public void checkAvailableLoadPage() throws InterruptedException {
+        Assert.assertTrue(availableLoadsAllPage.getTitle("Payments").contains("Payments"));
+        boolean isPresentLoad = availableLoadsAllPage.isElementPresent("path", availableLoadsAllPage.availableCardMap.get("numberOfLoad"));
+        if (isPresentLoad) {
+            Assert.assertTrue(Utils.isInteger(availableLoadsAllPage.getElementText("path", availableLoadsAllPage.allNumber)));
+        } else {
+            Assert.assertEquals(availableLoadsAllPage.getElementText("path", availableLoadsAllPage.noLoad), "Please try another type of load or let us know what you like and we'll text you loads that match your preferences.");
+        }
     }
 
 }
