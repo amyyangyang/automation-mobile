3bf1d6ca2ac10be73abfae59c7a8f59d9d8a502d
SergiiNik <nikitin.sergii@nexttrucking.com>
2019-08-09 10:50:57 +0800

    NTE-10405 add TC for Payment Details page

--- src/test/java/testcases/owneroperator/PaymentsTest.java
+++ src/test/java/testcases/owneroperator/PaymentsTest.java
@@ -41,4 +41,23 @@ public class PaymentsTest extends SetProperty {
         }
     }
 
+    @Test
+    public void checkPaymentDetailsPage() throws InterruptedException {
+        Assert.assertTrue(pageProperty.getTextByName("Payments").contains("Payments"));
+        boolean isPresentPayments = pageProperty.isElementPresent("path", paymentsPage.paymentList.get("firstPayment"));
+        if (isPresentPayments) {
+            pageProperty.clickElement(paymentsPage.paymentList.get("firstPayment"));
+            Assert.assertTrue(pageProperty.isTextPresent("Locations"));
+            Assert.assertTrue(pageProperty.isTextPresent("Details for"));
+            pageProperty.clickAnyElementByName("Details");
+            Assert.assertTrue(pageProperty.isTextPresent("You'll Make"));
+            Assert.assertTrue(pageProperty.isTextPresent("Rate Contract"));
+            Assert.assertTrue(pageProperty.isTextPresent("Proof of Delivery"));
+            Assert.assertTrue(pageProperty.isTextPresent("Equipment"));
+            signInPage.clickBackButton();
+        } else {
+            Assert.assertEquals(pageProperty.getTextByName("Book"), "Book a load below so we can start paying you tons of money!");
+        }
+    }
+
 }
