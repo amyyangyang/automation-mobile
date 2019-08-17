8bafe94c63eccf7a615c7279b65301819d449672
SergiiNik <nikitin.sergii@nexttrucking.com>
2019-08-17 15:47:28 +0800

    Fixing signUp() and editDriverInfo() Tests

--- src/test/java/testcases/dispatcher/DispatcherMyDriversTest.java
+++ src/test/java/testcases/dispatcher/DispatcherMyDriversTest.java
@@ -30,9 +30,8 @@ public class DispatcherMyDriversTest extends SetProperty {
         signInPage = new SignInPage(driver, attributeName);
         myDriversPage = new MyDriversPage(driver, attributeName);
         // SignIn as Dispatcher
-//        signInPage.signIn(getTestData("dispatcherEmail"), getTestData("dispatcherPassword"));
-//        availableLoadsAllPage.clickMenuButtonFirstLevel("My Drivers");
-        Thread.sleep(30000);
+        signInPage.signIn(getTestData("dispatcherEmail"), getTestData("dispatcherPassword"));
+        availableLoadsAllPage.clickMenuButtonFirstLevel("My Drivers");
     }
 
 
